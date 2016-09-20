ccbc5b0f07071fa4bbb356bf6798d70518bfa897
jpgermano <jader.gp15@gmail.com>
2016-09-20 19:25:30 -0300

    Request

--- aula4/request_controller.php
+++ aula4/request_controller.php
@@ -3,15 +3,19 @@ class RequestController
 {
 	const VALID_METHODS = array('GET', 'POST', 'PUT', 'DELETE');
 	const VALID_PROTOCOL = array('HTTP/1.1', 'HTTPS/1.1', 'HTTP/1.0',' HTTPS/1.0');
+
 	public function create_request($request_info)
 	{
-		if(!self::is_valid_method($request_info['REQUEST_METHOD']))
-		{
+		if(!self::is_valid_method($request_info['REQUEST_METHOD'])){
 			return array("code" => "405", "message" => "method not allowed");
 		}	
-		if(!self::is_valid_protocol($request_info['SERVER_PROTOCOL']))
-		{
+
+		if(!self::is_valid_protocol($request_info['SERVER_PROTOCOL'])){
 			return array("code" => "406", "message" => "protocol not allowed");
+		}
+
+		if(!self::is_valid_client_addr($request_info['REMOTE_ADDR'])){
+			return array("code" => "400", "message" => "Bad Request");
 		}
 	//	$request_info['REMOTE_ADDR'];
 	//	$request_info['SERVER_ADDR'];
@@ -31,10 +35,25 @@ class RequestController
 		
 		return true;
 	}
-	public function is_valid_protocol($protocol)
-	{
+	public function is_valid_protocol($protocol){
 		if(is_null($protocol) || !in_array($protocol, self::VALID_PROTOCOL))
 			return false;
 		return true;
+	}
+
+	public function is_valid_client_addr($client_addr){
+		if(is_null($remote_address) || filter_var($remote_address, FILTER_VALIDATE_IP) === FALSE){
+			
+            return false;
+		}
+		return true;
+	}
+
+	public function is_valid_path($path){
+		if(!is_dir($path)){
+			return false;
+		}
+		return true;
+		
 	}
 }
